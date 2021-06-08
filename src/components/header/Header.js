b63ebbc94d9fff59d3644f1f1226477aea13c39b
Kushal Agrawal <kushal_agrawal@cargill.com>
2021-06-09 00:12:09 +0530

    :art: :lipstick: style : css changes in nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,6 +6,9 @@ import {
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FcMenu } from "react-icons/fc";
+import { RiAdminLine } from "react-icons/ri";
+import { FaHeart } from "react-icons/fa";
+import { BiDonateBlood } from "react-icons/bi";
 
 import Sidebar from '../sidebar/SidebarPanel'
 
@@ -24,17 +27,26 @@ function Header(props) {
                 :
                 <></>
               }
-              <Navbar.Brand href="/home">Kasbai</Navbar.Brand>
+              <Navbar.Brand href="/home" style={{ fontSize: '25px' }}>Kasbai</Navbar.Brand>
               {
                 props.loggedIn?
                   <></> 
                 :
                 <>
                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav" style={{ justifyContent: 'flex-end' }}>
-                    <Nav >
+                  <Navbar.Collapse id="basic-navbar-nav" style={{ justifyContent: 'flex-end' , backgroundColor: '#ffffff' }}>
+                    <Nav style={{ fontSize: '20px' }} >
                       <Nav.Link as={Link} onClick={() => props.setLoginTrue(true)} className="custTag cust1" to="/home">
-                        Login
+                        <RiAdminLine size={25} style={{ marginRight: '5px' }}/>
+                        Admin Login
+                      </Nav.Link>
+                      <Nav.Link as={Link} onClick={() => props.setLoginTrue(true)} className="custTag cust1" to="/home">
+                        <BiDonateBlood size={25} style={{ marginRight: '5px' }}/>
+                        Donor Login
+                      </Nav.Link>
+                      <Nav.Link as={Link} onClick={() => props.setLoginTrue(true)} className="custTag cust1" style={{  color : 'red', padding: '5px', border: '1px dashed red', borderRadius: '10px' }} to="/home">
+                        <FaHeart size={20} style={{ marginRight: '5px' }}/>
+                        Donate Now
                       </Nav.Link>
                     </Nav>
                   </Navbar.Collapse>
