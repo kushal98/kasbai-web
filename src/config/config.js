c9cf5f72ee7cef8cd312adcf685b3ea0cb3cb6b1
Kushal Agrawal <kushal_agrawal@cargill.com>
2021-07-11 16:10:05 +0530

    :rocket: :sparkles: enhancement : add env secrets in github

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY || '${{ secrets.REACT_APP_API_KEY }}',
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN || '${{ secrets.REACT_APP_AUTH_DOMAIN }}',
+    projectId: process.env.REACT_APP_PROJECT_ID || '${{ secrets.REACT_APP_PROJECT_ID }}',
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET || '${{ secrets.REACT_APP_STORAGE_BUCKET }}',
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || '${{ secrets.REACT_APP_MESSAGING_SENDER_ID }}',
+    appId: process.env.REACT_APP_APP_ID || '${{ secrets.REACT_APP_APP_ID }}',
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID || '${{ secrets.REACT_APP_MEASUREMENT_ID }}'
   };
 
 firebase.initializeApp(firebaseConfig);
